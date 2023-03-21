203834b99384994381a0c543277e903403efbce0
s24945 <s24945@pjwstk.edu.pl>
2023-03-21 20:14:10 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 #zadanie1
 
+
 #zadanie2
-a = int(input("Podaj pierwszą liczbe: "))
-b = int(input("Podaj drugą liczbe: "))
-znak = input("Podaj znak: ")
-
-if znak == "+":
-    rownanie = a+b
-elif znak == "-"  :
-    rownanie = a-b
-elif znak == "*":
-    rownanie = a*b
-elif znak == "/":
-    rownanie = a/b
-else:
-    print("Podano zly znak")
-    rownanie = "---"
-
-print(a,znak,b,"=",rownanie)
 
 #zadanie3
---
+++ zad2.py
@@ -0,0 +1,17 @@
+a = int(input("Podaj pierwszą liczbe: "))
+b = int(input("Podaj drugą liczbe: "))
+znak = input("Podaj znak: ")
+
+if znak == "+":
+    rownanie = a+b
+elif znak == "-"  :
+    rownanie = a-b
+elif znak == "*":
+    rownanie = a*b
+elif znak == "/":
+    rownanie = a/b
+else:
+    print("Podano zly znak")
+    rownanie = "---"
+
+print(a,znak,b,"=",rownanie)
